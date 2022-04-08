af2c2f3e811cdf62dc05340f08052ee620708d8f
jason79619@gmail.com <jason79619@gmail.com>
2022-04-08 17:47:08 +0800

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Please inpur your name");
+            string name = Console.ReadLine();
+
+           MessageBox.Show("HELLO"+name);
+
+            System.Windows.Forms.MessageBox.Show("HELLOHELLO"+name);
+        }
+    }
+}
